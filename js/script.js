5bed0c6f69e659c31b81917c202b8db5452b721e
felipef210 <rfelipe321@live.com>
2024-03-12 12:59:37 -0300

    Commit 12/03/2024

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 function changeSlide(index) {
     const slides = document.querySelectorAll('.slide');
+    const slideWidth = slides[0].offsetWidth; // Largura de cada slide
+
     slides.forEach((slide, i) => {
-        if (i === index) {
-            slide.style.display = 'block';
-        } else {
-            slide.style.display = 'none';
-        }
+        const distance = (i - index) * slideWidth; // Distância que o slide deve se mover
+        slide.style.transform = `translateX(${distance}px)`; // Move o slide horizontalmente
     });
 
     const buttons = document.querySelectorAll('.manual-btn');
@@ -17,3 +16,11 @@ function changeSlide(index) {
         }
     });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    changeSlide(0); // Altere para 0 se quiser começar com o primeiro slide
+
+    // Adicione a classe 'active' ao botão correspondente ao slide inicial
+    // const initialButton = document.querySelector('.manual-btn:nth-child(1)'); // Altere para 1 se quiser começar com o primeiro slide
+    // initialButton.classList.add('active');
+});
