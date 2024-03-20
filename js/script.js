a0c117fd45913469f503329e4f50cb2cd197d3de
felipef210 <rfelipe321@live.com>
2024-03-20 12:52:32 -0300

    Commit 20/03/2024

--- js/script.js
+++ js/script.js
@@ -11,6 +11,18 @@ function menuShow() {
             menuMobile.style.display = 'none';
         }, 500);
     }
+
+    let btnIcon = document.getElementById('btn-menu').getElementsByTagName('i')[0];
+    if (btnIcon.classList.contains('fa-bars')) {
+        btnIcon.classList.remove('fa-bars');
+        btnIcon.classList.add('fa-times');
+    } 
+    else {
+        setTimeout(function() {
+            btnIcon.classList.remove('fa-times');
+            btnIcon.classList.add('fa-bars');
+        }, 500);
+    }
 }
 // Fim Menu Responsivo
 
